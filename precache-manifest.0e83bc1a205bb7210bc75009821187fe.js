bd2e7befebd4b65de474346c8a5eed134517dc11
yuriy82791 <yuriy82791@gmail.com>
2020-04-17 23:49:36 +0300

    Updates

---
+++ precache-manifest.0e83bc1a205bb7210bc75009821187fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b26ef59889190984a2f863db35ff944",
+    "url": "/form-app/index.html"
+  },
+  {
+    "revision": "70231b78c9d8115b7c9f",
+    "url": "/form-app/static/css/main.fd29cb2c.chunk.css"
+  },
+  {
+    "revision": "2b0eec8fed7044495569",
+    "url": "/form-app/static/js/2.e8304267.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/form-app/static/js/2.e8304267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70231b78c9d8115b7c9f",
+    "url": "/form-app/static/js/main.43a24628.chunk.js"
+  },
+  {
+    "revision": "5a1b31c2a4d15f99546f",
+    "url": "/form-app/static/js/runtime-main.81cfb7c1.js"
+  },
+  {
+    "revision": "e813679fdc6e17a866d2284cd72d416e",
+    "url": "/form-app/static/media/padlock.e813679f.svg"
+  }
+]);
